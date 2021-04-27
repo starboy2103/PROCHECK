c0e3df2f5febbab7d840576d4088ea00bab0632c
Kowshal <kowshal2103@gmail.com>
2021-04-27 23:08:49 +0530

    Total change. Please change the values

--- js/fixed.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", function(){
-    const main = document.querySelector("div.main");
-    const front = document.querySelector("div.t-front");
-    if(front.getBoundingClientRect().bottom >= main.getBoundingClientRect().top)
-    {
-        main.style.position = 'fixed';
-    }
-});
--- js/relative.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll",function() {
-    const main= document.querySelector("div.main");
-    const title= document.querySelector("div.title");
-    if(main.getBoundingClientRect().bottom>=title.getBoundingClientRect().bottom)
-    {
-        main.style.position='relative';
-    }
-});
